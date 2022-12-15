5eaa971a5898ab9e13303a6bd28d1966a9356b03
Ivisonj <ivison_j@outlook.com>
2022-12-14 21:47:51 -0300

    projeto concluído

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -8,28 +8,36 @@ import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
 
+import { api } from '../../services/api';
+
 import { Column, Container, CriarText, EsqueciText, Row, SubtitleLogin, Title, TitleLogin, Wrapper } from "./styles";
 
 const schema = yup.object({
     email: yup.string().email('email não é válido').required('Campo obrigatório'),
-    password: yup.string().min(3, 'No minimo 3 caracteres').required('obrigatório'),
+    password: yup.string().min(3, 'No minimo 3 caracteres').required('Campo obrigatório'),
 }).required();
 
 const Login = () => {
     const navigate = useNavigate();
 
-    const { control, handleSubmit, formState: {errors, isValid} } = useForm({
+    const { control, handleSubmit, formState: {errors} } = useForm({
         resolver: yupResolver(schema),
         mode: 'onChange',
     });
-    console.log(isValid, errors)
-
-    const onSubmit = date => console.log(date);
-
-    const handleClickSignIn = () => {
-        navigate('/feed')
+    
+    const onSubmit = async formDate => {
+        try{
+            const { data } = await api.get(`users?email=${formDate.email}&senha=${formDate.password}`);
+            if(data.length === 1) {
+                navigate('/feed')
+            } else {
+                alert('Email ou senha inválido')
+            }
+        } catch {
+         
+            alert('Houve um erro, tente novamente.')
+        }
     }
-
 
     return (<>
         <Header />
